db5e098489e98812a642b00219e5f0887d34772a
sheetal31 <sheetaln803@gmail.com>
2022-11-08 14:48:46 +0530

    sfgfgfgh

---
+++ Selenium/src/test/java/org/project/Demo.java
@@ -0,0 +1,26 @@
+package org.project;
+
+import java.awt.RenderingHints.Key;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']")).click();
+		driver.findElement(By.xpath("//input[@class='_3704LK']")).sendKeys("Mobiles"+Keys.ENTER);
+		System.out.print("hdihgfkhfgh");
+	}
+
+}
